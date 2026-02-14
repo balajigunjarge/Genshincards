34f59c7452824f5a443bcee4eccc35e11d5254b8
Balaji <balajirameshgunjarge@gmail.com>
2026-02-14 16:15:06 +0530

    feat: Initialize React project with Vite, core components, and game-related assets.

---
+++ src/App.jsx
@@ -0,0 +1,127 @@
+import Card from "./components/card";
+
+
+
+const genshinCharacters = [
+  {
+    id:1,
+    name: "Venti",
+    about: "A free-spirited, wine-loving bard in Mondstadt who is actually the mortal vessel of Barbatos, the Anemo Archon.",
+    element: "Anemo",
+    region: "Mondstadt",
+    image: "../src/assets/genshin/Venti.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Venti_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/mondstadt?char=7"
+  },
+  {
+    id:2,
+    name: "Zhongli",
+    about: "A calm and knowledgeable consultant for the Wangsheng Funeral Parlor, secretly the Geo Archon Morax.",
+    element: "Geo",
+    region: "Liyue",
+    image: "../src/assets/genshin/Zhongli.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Zhongli_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/liyue?char=9"
+  },
+  {
+    id:3,
+    name: "Raiden Shogun",
+    about: "The undisputed supreme ruler of Inazuma and vessel of Beelzebul, the Electro Archon who pursues unchanging eternity.",
+    element: "Electro",
+    region: "Inazuma",
+    image: "../src/assets/genshin/Raiden Shogun.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Raiden Shogun_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/inazuma?char=4"
+  },
+  {
+    id:4,
+    name: "Nahida",
+    about: "The current Dendro Archon, known as Lesser Lord Kusanali, who possesses a deep connection to the Irminsul tree.",
+    element: "Dendro",
+    region: "Sumeru",
+    image: "../src/assets/genshin/Nahida.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Nahida_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/sumeru?char=6"
+  },
+  {
+    id:5,
+    name: "Furina",
+    about: "The flamboyant and theatrical former Hydro Archon of Fontaine who played a centuries-long role to save her people from prophecy.",
+    element: "Hydro",
+    region: "Fontaine",
+    image: "../src/assets/genshin/Furina.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Furina_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/Fontaine?char=6"
+  },
+  {
+    id:6,
+    name: "Kamisato Ayaka",
+    about: "The elegant, noble, and highly capable daughter of the Yashiro Commission's Kamisato Clan.",
+    element: "Cryo",
+    region: "Inazuma",
+    image: "../src/assets/genshin/Kamisato Ayaka.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Kamisato Ayaka_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/inazuma?char=0"
+  },
+  {
+    id:7,
+    name: "Diluc",
+    about: "The wealthy owner of the Dawn Winery who moonlights as Mondstadt's shadowy protector, the Darknight Hero.",
+    element: "Pyro",
+    region: "Mondstadt",
+    image: "../src/assets/genshin/Diluc.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Diluc_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/mondstadt?char=5"
+  },
+  {
+    id:8,
+    name: "Yelan",
+    about: "A mysterious and elusive intelligence agent who claims to work for Liyue's Ministry of Civil Affairs.",
+    element: "Hydro",
+    region: "Liyue",
+    image: "../src/assets/genshin/Yelan.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Yelan_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/liyue?char=16"
+  },
+  {
+    id:9, 
+    name: "Alhaitham",
+    about: "The highly rational and capable Scribe of the Sumeru Akademiya who prefers a quiet, comfortable life.",
+    element: "Dendro",
+    region: "Sumeru",
+    image: "../src/assets/genshin/Alhaitham.PNG",
+    backgroundImage: "../src/assets/genshin/UI_NameCardPic_Alhaitham_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/sumeru?char=10"
+  },
+  {
+    id:10,
+    name: "Navia",
+    about: "The optimistic, determined, and stylish President of Fontaine's Spina di Rosula.",
+    element: "Geo",
+    region: "Fontaine",
+    image: "../src/assets/genshin/Navia.PNG",
+    backgroundImage:"../src/assets/genshin/UI_NameCardPic_Navia_P.png",
+    link:"https://genshin.hoyoverse.com/en/character/Fontaine?char=7"
+  }
+];
+
+
+
+const App = () => {
+  return (
+    <div className='home'>
+      
+
+      { genshinCharacters.map(function(elem){
+        return (
+         
+            
+      <Card key={elem.id}  name={elem.name} region={elem.region} about={elem.about} image={elem.image} backgroundImage={elem.backgroundImage} link={elem.link} />
+        )
+    })}
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/card.jsx
@@ -0,0 +1,21 @@
+const Card = (props) => {
+  return (
+    <div className="card">
+     <img src={props.backgroundImage} alt="" className="background-img"/>
+      <div className="top-div">
+        
+      <img src={props.image} alt="" className="icon-img"/><h1>{props.id}{props.name}<br/><section className="region">{props.region}</section></h1>
+     </div>
+      <p>{props.about}</p>
+
+      <a href={props.link} target="_blank">
+        <button>Learn More</button>
+      </a>
+      
+      
+      
+    </div>
+  )
+}
+
+export default Card 
