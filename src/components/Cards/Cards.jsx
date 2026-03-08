9f002a5890a00b55017a0fb575e8870075b5f521
balaji <balajigunjargedev@gmail.com>
2026-03-08 06:10:09 -0700

    fixes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -9,8 +9,8 @@ const Cards = (props) => {
        <div className=" flex absolute top-1/4 left-6">
          <div >  <img className="w-18 object-cover object-center rounded-2xl " src={props.image}/>
      </div>
-   <div className="p-3"> <h1 className={`${styles.title} text-[#ffffff]`}>{props.name}</h1>
-   <section className="flex gap-3"><p className={` bg-[#0000002f] rounded-lg backdrop-blur-2xl px-3 py-0.5 ${styles[props.element]}`}  >{props.element} </p>  <p>{props.region}</p ></section>
+   <div className="p-1"> <h1 className={`${styles.title} text-[#ffffff]`}>{props.name}</h1>
+   <section className="flex gap-3 mt-2"><p className={` bg-[#0000002f] rounded-lg backdrop-blur-2xl px-3 py-0.5 ${styles[props.element]}`}  >{props.element} </p>  <p>{props.region}</p ></section>
         </div>
 
        </div>
